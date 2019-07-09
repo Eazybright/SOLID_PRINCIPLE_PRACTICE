6c4c8f31a6cd8eac0fb93b568c7ace8b968eede5
Ali Shah <ali.shah@footasylum.com>
2019-07-10 00:24:29 +0100

    added the shapes calculator to project

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::post('order/{product_id}/process', ProcessOrdersController::class);
+
+Route::get('area', function(\App\Patterns\AreaCalculator $areaCalculator) {
+
+    $square = new \App\Patterns\Square(10,10);
+    $triangle = new \App\Patterns\Triangle(10,6);
+    $circle = new \App\Patterns\Circle(10);
+
+    return $areaCalculator->calculate($circle);
+
+});
