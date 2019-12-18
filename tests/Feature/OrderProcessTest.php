a885fb6fd68c5089a552994810bd0e492f5b123c
Omoniyi Omotoso <omoniyi.o@e360africa.com>
2019-12-18 12:39:56 +0100

    resolve merge conflict

--- tests/Feature/OrderProcessTest.php
+++ tests/Feature/OrderProcessTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 use App\Models\Stock;
 use App\Models\Product;
@@ -14,31 +13,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class OrderProcessTest extends TestCase
 {
     // use RefreshDatabase;
-
-    /** @test */
-    public function a_user_order_can_be_processed(): void
-    {
-        $product = factory(\App\Models\Product::class)->create();
-
-        $stock = factory(\App\Models\Stock::class)->create([
-            'product_id' => $product->id
-        ]);
-
-       $response =  $this->post("/order/{$product->id}/process", [
-            'payment_method' => 'stripe'
-        ])->assertOk()->json();
-
-       $this->assertArrayHasKey('payment_message', $response);
-       $this->assertArrayHasKey('discounted_price', $response);
-       $this->assertArrayHasKey('original_price', $response);
-       $this->assertArrayHasKey('message', $response);
-
-        $this->assertDatabaseHas('stocks', [
-            'quantity' => $stock->quantity - 1
-        ]);
-
-    }
- 
     /**
      * Proceess user orders.
      *
@@ -69,23 +43,24 @@ class OrderProcessTest extends TestCase
         ]);
     }
 
-    public function an_error_is_thrown_when_product_is_out_of_stock(): void
+    /** @test */
+    public function an_exception_is_thrown_if_stock_is_less_than_one(): void
     {
         $this->expectException(ValidationException::class);
 
-        //create product and stock
-        $product = factory(Product::class)->create();
+        $product = factory(\App\Models\Product::class)->create();
 
-        $stock = factory(Stock::class)->create([
+        factory(\App\Models\Stock::class)->create([
             'quantity' => 0,
             'product_id' => $product->id
+        ]);
+
+        $response = $this->withoutExceptionHandling()->post("order/{$product->id}/process", [
+            'payment_method' => 'stripe'
         ]);
         $this->withoutExceptionHandling()
             ->post("/order/{$product->id}/process", [
                 'payment_method' => 'stripe'
             ]);
-        $response = $this->withoutExceptionHandling()->post("order/{$product->id}/process", [
-            'payment_method' => 'stripe'
-        ]);
     }
 }
